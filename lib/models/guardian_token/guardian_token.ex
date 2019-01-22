72e58329c448bd2a626a8771b719c892f8ca979c
David Kerr <kerrd89@gmail.com>
2019-01-21 18:15:45 -0700

    create guardian token db record: '

---
+++ lib/models/guardian_token/guardian_token.ex
@@ -0,0 +1,16 @@
+defmodule NbaLinesServer.GuardianToken do
+  use NbaLinesServer.Web, :model
+
+  @primary_key {:jti, :string, []}
+  @derive {Phoenix.Param, key: :jti}
+  schema "guardian_tokens" do
+    field :aud, :string
+    field :iss, :string
+    field :sub, :string
+    field :exp, :integer
+    field :jwt, :string
+    field :claims, :map
+
+    timestamps()
+  end
+end
