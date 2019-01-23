bbf5f6444895c4f54761fc6885d8e6a09520ec25
David Kerr <kerrd89@gmail.com>
2019-01-22 19:14:30 -0700

    fix session_controller tests to test api funtionality

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -13,27 +13,16 @@ defmodule NbaLinesServer.SessionControllerTest do
     {:ok, %{user: user}}
   end
 
-  test "POST /authenicate creates a session for a user",
-    %{user: user}
-  do
-    # First ensure we have no current guardian tokens.
-    tokens = Repo.all(GuardianToken)
-    assert tokens == []
+  test "POST /login should fail without proper credentials" do
+    conn = post(build_conn(), session_path(build_conn(), :login), %{email: "fake", password: "fake"})
+    assert json_response(conn, 302)
+  end
 
-    # confirm we are getting an html response on a valid login attempt
-    conn = post(build_conn(), session_path(build_conn(), :create),
-      %{"session" => %{"email" => user.email, "password" => "password"}})
+  test "POST /login should give token with proper credentials", %{user: user} do
+    conn = build_conn()
+    |> put_req_header("content-type", "application/json")
+    |> post(session_path(build_conn(), :login), %{email: user.email, password: user.password})
 
-    assert html_response(conn, 302)
-
-    tokens = Repo.all(GuardianToken)
-    assert tokens != []
-
-    # Ensure we have tokens properly set with issuer and audience.
-    # These are needed by Guardian DB to verify the user is valid.
-    Enum.each(tokens, fn(token) ->
-      assert token.aud
-      assert token.iss
-    end)
+    assert json_response(conn, 200)
   end
 end
