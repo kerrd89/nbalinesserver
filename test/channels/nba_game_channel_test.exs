a5457251a4e22b6d1b6508df13a6b146e2f8aa3e
David Kerr <kerrd89@gmail.com>
2019-01-24 12:56:22 -0700

    fix tests, asserting joining gets nba_games for today

--- test/channels/nba_game_channel_test.exs
+++ test/channels/nba_game_channel_test.exs
@@ -26,14 +26,22 @@ defmodule NbaLinesServer.NbaGameChannelTest do
         end
   
         test "authenticated_users recieve nba_games when they join the channel", %{user: user} do
+            create(:nba_game)
+            create(:nba_game)
+            create(:nba_game)
+            create(:nba_game)
+
             {:ok, jwt, _full_claims} = NbaLinesServer.Guardian.encode_and_sign(user)
+
             socket = socket(UserSocket)
-            {:ok, payload, _socket} = subscribe_and_join(socket, NbaGameChannel, "nba_games", %{"guardian_token" => "#{jwt}"})
+            {:ok, %{nba_games: nba_games_resp}, _socket} = subscribe_and_join(socket, NbaGameChannel, "nba_games", %{"guardian_token" => "#{jwt}"})
     
             # test alert_helper payload in alert_helper_test, here we just check it is returned
             nba_games = Date.utc_today() |> NbaGame.Api.get_nba_games_by_date()
-    
-            assert %{nba_games: nba_games} == payload
+
+            assert Enum.count(nba_games) == 4
+            assert Enum.count(nba_games_resp) == 4
+            assert nba_games_resp == nba_games
         end
     end
 end
